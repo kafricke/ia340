8b7f2cd8bbc44e476a7ee160c3b27d7c55bcce63
kafricke <122939348+kafricke@users.noreply.github.com>
2023-09-06 17:37:31 -0400

    Create lab2.sql

---
+++ lab2.sql
@@ -0,0 +1,64 @@
+-- Create schemas
+
+-- Create tables
+CREATE TABLE IF NOT EXISTS professor
+(
+    id INTEGER NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS student
+(
+    s_email VARCHAR(50) NOT NULL,
+    s_name VARCHAR(50),
+    "major " VARCHAR(20),
+    PRIMARY KEY(s_email)
+);
+
+CREATE TABLE IF NOT EXISTS course
+(
+    c_email VARCHAR(20) NOT NULL,
+    c_name VARCHAR(50),
+    room VARCHAR(20) NOT NULL,
+    p_email2 VARCHAR(50) NOT NULL,
+    PRIMARY KEY(c_email)
+);
+
+CREATE TABLE IF NOT EXISTS enroll
+(
+    c_email VARCHAR(20) NOT NULL,
+    s_email VARCHAR(50) NOT NULL,
+    PRIMARY KEY(c_email, s_email)
+);
+
+CREATE TABLE IF NOT EXISTS professor
+(
+    p_email VARCHAR(50) NOT NULL,
+    p_name VARCHAR(50),
+    office VARCHAR(20) NOT NULL UNIQUE,
+    PRIMARY KEY(p_email)
+);
+
+
+-- Create FKs
+ALTER TABLE course
+    ADD    FOREIGN KEY (p_email2)
+    REFERENCES professor(p_email)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE enroll
+    ADD    FOREIGN KEY (c_email)
+    REFERENCES course(c_email)
+    MATCH SIMPLE
+;
+    
+ALTER TABLE enroll
+    ADD    FOREIGN KEY (s_email)
+    REFERENCES student(s_email)
+    MATCH SIMPLE
+;
+    
+
+-- Create Indexes
+
