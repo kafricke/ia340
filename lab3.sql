0dcb4f0467c2dc5a5c035d7c2983a5205a620aa0
kafricke <122939348+kafricke@users.noreply.github.com>
2023-09-13 17:25:07 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -10,3 +10,7 @@ insert into professor(p_email,p_name,office)
 values  ('pl@jmu.edu','p1','o1'),
 	('p2@jmu.edu','p2','o2')
 
+insert into course(c_number, c_name, room,p_email)
+values  ('c1','postgresql','r1','p1@jmu.edu'),
+		('c2','mongodb','r2','p2@jmu.edu'),
+		('c3', 'twitter', 'r1', 'p1@jmu.edu')
