3d3e519556ad5aa777c1b9d0384f947e3a3ea8bf
kafricke <122939348+kafricke@users.noreply.github.com>
2023-09-13 17:47:58 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -5,7 +5,6 @@ values('s1@jmu.edu', 's1', 'IA' ),
 	    ('s3@jmu.edu', 's3', 'ISAT'),
 	    ('s4@jmu.edu', 's4', 'ISAT')
 
-
 insert into professor(p_email,p_name,office)
 values  ('pl@jmu.edu','p1','o1'),
 	('p2@jmu.edu','p2','o2')
@@ -14,3 +13,12 @@ insert into course(c_number, c_name, room,p_email)
 values  ('c1','postgresql','r1','p1@jmu.edu'),
 		('c2','mongodb','r2','p2@jmu.edu'),
 		('c3', 'twitter', 'r1', 'p1@jmu.edu')
+
+insert into enroll(s_email,c_number)
+values
+('s1@jmu.edu','c1'),
+('s2@jmu.edu','c1'),
+('s3@jmu.edu','c1'),
+('s4@jmu.edu','c2'),
+('s2@jmu.edu','c3'),
+('s3@jmu.edu','c3')
