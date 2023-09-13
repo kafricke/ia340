ed861b7a2808a87183e0137fdc6892942ea5e7e9
kafricke <122939348+kafricke@users.noreply.github.com>
2023-09-13 17:17:33 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -4,4 +4,9 @@ values('s1@jmu.edu', 's1', 'IA' ),
       ('s2@jmu.edu', 's2', 'IA'),
 	    ('s3@jmu.edu', 's3', 'ISAT'),
 	    ('s4@jmu.edu', 's4', 'ISAT')
- 
+
+
+insert into professor(p_email,p_name,office)
+values  ('pl@jmu.edu','p1','o1'),
+	('p2@jmu.edu','p2','o2')
+
