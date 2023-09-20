6b8728960aa91cffc09c50ab8a20a458b4a4fd66
kafricke <122939348+kafricke@users.noreply.github.com>
2023-09-20 17:49:13 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -26,7 +26,7 @@ values
 --q2.5
 --insert professor first, because foreign key in the course table
 insert into professor(p_email,p_name,office)
-values  ('p3@jmu.edu','p1','o3') ;
+values  ('p3@jmu.edu','p3','o3') ;
 
 insert into course(c_number, c_name, room,p_email)
 values('c4','facebook','r1','p3@jmu.edu')
@@ -43,7 +43,7 @@ where p_email = 'p1@jmu.edu'
 --q2.7
 select * from enroll
 
---q.8
+--q2.8
 select c_number, count(*) as num_student
 
 from enroll
@@ -51,10 +51,20 @@ group by c_number
 order by num_student desc
 limit 1
 
---q.9
+--q2.9
 select professor.p_name, 
 	course.c_name
 from professor 
 inner join course 
 on professor.p_email = course.p_email
+
+--q2.10 
+select professor.p_name,
+		count(course.c_name) as num_course
+from professor 
+inner join course
+on professor.p_email = course.p_email 
+group by professor.p_name 
+order by num_course desc
+limit 1
 
